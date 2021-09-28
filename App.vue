ef23493b02f0609f27d09704fa5e0533248ecb90
deepinwst <wanshitao@donews.com>
2021-09-28 14:00:53 +0800

    形成项目结构

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+import MyHeader from './components/MyHeader'
+import MyList from './components/MyList'
+import MyFooter from './components/MyFooter'
+
+export default {
+    name: "App",
+    components: {MyHeader, MyList, MyFooter}
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/MyList.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+import MyItem from './MyItem'
+
+export default {
+    name: 'MyList',
+    components: {MyItem}
+}
+</script>
+
+<style>
+
+</style>
